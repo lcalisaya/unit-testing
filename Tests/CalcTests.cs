3efc958dcab0e92255dab5024f3a49238d18afa5
Lucía Calisaya <calisaya.lucia@gmail.com>
2021-09-27 18:13:08 -0300

    Con DataRow se pueden probar más escenarios de manera práctica

--- Tests/CalcTests.cs
+++ Tests/CalcTests.cs
@@ -6,22 +6,20 @@ namespace Tests
     [TestClass]
     public class CalcTests
     {
+        [DataRow(5, 6,11)]
+        [DataRow(15, -6, 9)]
+        [DataRow(-13, 47, 34)]
         [TestMethod]
-        public void Sum_ShouldCalcAndReturnValue() //A descriptive name
+        public void Sum_ShouldCalcAndReturnValue(int a, int b, int expected) //A descriptive name
         {
-            //Act: Setting and initializing variables
-            int a = 5;
-            int b = 6;
-            int expected = 11;
+            //Arrange: Setting and initializing variables
+            //Variables now declared as parameters
 
-            //Arrange: The method is used
+            //Act : The method is used
             int actual = Calcs.Sum(a, b);
 
             //Assert: It will be validated that the result is the expected
             Assert.AreEqual(expected, actual);
-
-            //"Equals" compares references
-            //Assert.Equals(expected, actual);
         }
     }
 }
