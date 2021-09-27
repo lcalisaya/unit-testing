b0ea8514fcaef1a69ad31039a6a1c35792b8244e
Lucía Calisaya <calisaya.lucia@gmail.com>
2021-09-27 18:41:48 -0300

    Se verifica que dada cierta entrada se arroje una excepción

--- Tests/CollectionSampleTests.cs
+++ Tests/CollectionSampleTests.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using UnitTestingMSTestIntro;
 
 namespace Tests
@@ -22,6 +23,23 @@ namespace Tests
             Assert.IsInstanceOfType(actualCustomer, typeof(Customer));
             Assert.AreEqual(expectedCustomer, actualCustomer);
 
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void GetCustomer_ShouldThrowIndexOutOfRangeException()
+        {
+            //Arrange
+            Customer[] customers = new Customer[2] { new Customer { FirstName = "Patricia", LastName = "Suarez"},
+                                                     new Customer { FirstName = "Agostina", LastName = "Fernandez" } };
+            int index = 1;
+            Customer expectedCustomer = customers[3];
+
+            //Act
+            Customer actualCustomer = CollectionsSample.GetCustomer(customers, index);
+
+            //Assert
+            //It's not neccesary here Assert method because ExpectedException decorator will give the response
         }
     }
 }
