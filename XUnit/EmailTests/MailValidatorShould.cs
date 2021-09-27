3d0853613970522c6e870d6988977d668dc5584d
Lucía Calisaya <calisaya.lucia@gmail.com>
2021-09-27 19:59:46 -0300

    Con los decoradores Theory y InlineData se definen más escenarios posibles de probar

--- XUnit/EmailTests/MailValidatorShould.cs
+++ XUnit/EmailTests/MailValidatorShould.cs
@@ -6,18 +6,49 @@ namespace EmailTests
 {
     public class MailValidatorShould
     {
+        //[Fact]
+        //public void ValidateValidEmails()
+        //{
+        //    //Arrange
+        //    var mailValidator = new MailValidator();
+        //    string emailAddress = "maildeprueba@gmail.com";
+
+        //    //Act
+        //    bool isValid = mailValidator.IsValidEmail(emailAddress);
+
+        //    //Assert
+        //    Assert.True(isValid, $"{emailAddress} is not a valid address.");
+
+        //}
+
         [Fact]
-        public void ValidateValidEmails()
+        public void InvalidateInvalidEmails()
         {
             //Arrange
             var mailValidator = new MailValidator();
-            string emailAddress = "maildeprueba@gmail.com";
+            string emailAddress = "invalid@invalid.invalid";
 
             //Act
             bool isValid = mailValidator.IsValidEmail(emailAddress);
 
             //Assert
-            Assert.True(isValid, $"{emailAddress} is not a valid address.");
+            Assert.False(isValid);
+
+        }
+
+        [Theory]
+        [InlineData("maildeprueba@gmail.com", true)]
+        [InlineData("invalid@invalid.invalid", false)]
+        public void ValidateValidEmails(string emailAddress, bool expected)
+        {
+            //Arrange
+            var mailValidator = new MailValidator();
+
+            //Act
+            bool actual = mailValidator.IsValidEmail(emailAddress);
+
+            //Assert
+            Assert.Equal(expected, actual);
 
         }
     }
